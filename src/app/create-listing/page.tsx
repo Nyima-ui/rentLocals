8e1f50017811901e2a404e4ca4d08781cff5c566
Tenzin Nyima <ntenzin492@gmail.com>
2025-12-31 10:15:32 +0530

    create listing form and listing page

--- src/app/create-listing/page.tsx
+++ src/app/create-listing/page.tsx
@@ -1,11 +1,116 @@
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { SearchBox } from "../page";
+import { SelectContent } from "@radix-ui/react-select";
 
+export function CreateListingForm() {
+  return (
+    <div className="my-14">
+      <h1 className="text-3xl mt-5">Post your listing</h1>
+      <form className="w-full max-w-xl py-5 rounded-md mt-5 px-5 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+        <FieldGroup className="mt-5">
+          <Field className="">
+            <FieldLabel className="text-base" htmlFor="select-category">
+              Choose category
+            </FieldLabel>
+            <Select required>
+              <SelectTrigger defaultValue="" id="select-category">
+                <SelectValue placeholder="Category" />
+              </SelectTrigger>
+              <SelectContent className="bg-gray-800 text-white rounded-sm max-w-lg max-md:max-w-md">
+                <SelectItem value="Party items"> Party items</SelectItem>
+                <SelectItem value="Electronics"> Electronics</SelectItem>
+                <SelectItem value="Car accessories">Car accessories</SelectItem>
+                <SelectItem value="Sports equipments">
+                  Sports equipments
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="title" className="text-base">
+              Title
+            </FieldLabel>
+            <Input id="title" placeholder="Listing name" required />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="description" className="text-base">
+              Description
+            </FieldLabel>
+            <Textarea
+              id="description"
+              className="h-38 resize-none"
+              placeholder="Describe your listing.."
+              required
+            />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="pictures" className="text-base">
+              Pictures
+            </FieldLabel>
+            <div className="grid grid-cols-3 gap-3 max-sm:grid-cols-2">
+              <Input type="file" accept="image/*" required />
+              <Input type="file" accept="image/*" />
+              <Input type="file" accept="image/*" />
+              <Input type="file" accept="image/*" />
+              <Input type="file" accept="image/*" />
+              <Input type="file" accept="image/*" />
+            </div>
+          </Field>
+          <FieldGroup>
+            <p className="text-base font-medium">Prices</p>
+            <div className="flex gap-5 max-[400px]:flex-wrap">
+              <Field>
+                <FieldLabel htmlFor="price-1-day">Price for 1 day</FieldLabel>
+                <Input id="price-1-day" placeholder="$" required />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="price-1-week">Price for 1 week</FieldLabel>
+                <Input id="price-1-week" placeholder="$" />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="price-1-month">
+                  Price for 1 month
+                </FieldLabel>
+                <Input id="price-1-month" placeholder="$" />
+              </Field>
+            </div>
+          </FieldGroup>
+          <Field>
+            <FieldLabel htmlFor="location" className="text-base">
+              Pickup location
+            </FieldLabel>
+            <Input id="location" placeholder="12 xyz ave.." required />
+            <Button variant="outline" className="max-w-xs">
+              Use my location
+            </Button>
+          </Field>
+          <Field>
+            <Button type="submit" className="max-w-xs cursor-pointer">
+              Post listing
+            </Button>
+          </Field>
+        </FieldGroup>
+      </form>
+    </div>
+  );
+}
 
 const CreateListing = () => {
   return (
-    <div className='text-red-400'>
-       <p className=''>create listing page</p>
+    <div className="max-w-6xl mx-auto max-xl:px-5">
+      <SearchBox />
+      <CreateListingForm />
     </div>
-  )
-}
+  );
+};
 
-export default CreateListing
+export default CreateListing;
--- src/app/listing/[id]/page.tsx
+++ src/app/listing/[id]/page.tsx
@@ -1,41 +1,154 @@
-import { SearchBox } from "@/app/page"
-import { ImageIcon } from "lucide-react"
+"use client";
+import { SearchBox } from "@/app/page";
+import Image from "next/image";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardTitle,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 
-
-export function ListingImage(){
-   return (
-     <div className="w-[56%] max-md:w-full">
-        <div className="bg-gray-400 h-100 w-full">
-          <ImageIcon className="text-gray-200" />
-        </div>
-        <ul className="flex mt-2 gap-2 justify-end">
-            <li className="bg-gray-400 size-15"><ImageIcon className="text-gray-200"/></li>
-            <li className="bg-gray-400 size-15"><ImageIcon className="text-gray-200"/></li>
-            <li className="bg-gray-400 size-15"><ImageIcon className="text-gray-200"/></li>
-        </ul>
-     </div>
-   )
+export function ListingImage() {
+  const [imageDisplayed, setImageDisplayed] = useState<string>("/file.svg");
+  return (
+    <div className="w-full max-md:w-full">
+      <div className="bg-gray-400 h-100 w-full">
+        <Image height={50} width={50} alt="some svg" src={imageDisplayed} />
+      </div>
+      <ul className="flex mt-2 gap-2 justify-end">
+        <li className="bg-gray-400 size-15">
+          <Image
+            height={50}
+            width={50}
+            alt="some svg"
+            src={"/file.svg"}
+            onClick={() => setImageDisplayed("/file.svg")}
+          />
+        </li>
+        <li className="bg-gray-400 size-15">
+          <Image
+            height={50}
+            width={50}
+            alt="some svg"
+            src={"/globe.svg"}
+            onClick={() => setImageDisplayed("/globe.svg")}
+          />
+        </li>
+        <li
+          className="bg-gray-400 size-15"
+          onClick={() => setImageDisplayed("/next.svg")}
+        >
+          <Image height={50} width={50} alt="some svg" src={"/next.svg"} />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export function ListingInfo(){
+export function ListingInfo() {
   return (
-     <div className="border grow border-green-400 bg-green-200">
-        <h1>Xp-pen innovator display 16 drawing tablet (15.6)</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto consectetur nostrum adipisci molestias eligendi facilis ratione tenetur doloribus iusto deleniti?</p>
-     </div>
-  )
+    <div className="grow space-y-3">
+      <h1 className="text-4xl font-medium leading-12">
+        Xp-pen innovator display 16 drawing tablet (15.6)
+      </h1>
+      <div className="flex items-center justify-between max-w-lg">
+        <label htmlFor="start-date" className="text-nowrap">
+          Start date:
+        </label>
+        <Input type="date" id="start-date" className="max-w-[320px]" />
+      </div>
+      <div className="flex items-center justify-between max-w-lg">
+        <label htmlFor="end-date" className="text-nowrap">
+          End date:
+        </label>
+        <Input type="date" id="end-date" className="max-w-[320px]" />
+      </div>
+      <p className="mt-10 max-w-xl">
+        <span className="text-lg block">About the item:</span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        consectetur nostrum adipisci molestias eligendi facilis ratione tenetur
+        doloribus iusto deleniti?
+      </p>
+      <div>
+        <h2 className="text-center text-3xl font-medium opacity-80 mt-6">
+          $27.50
+        </h2>
+        <p className="text-center text-gray-500">Price for 1 day</p>
+      </div>
+      <Button className="mx-auto block mt-5">Request booking</Button>
+      <div className="mt-10">
+        <p className="text-center text-lg">Price for all periods:</p>
+        <div className="flex gap-5 mt-3">
+          <div className="border grow text-center rounded-md py-1.5">
+            <p className="text-xl font-medium">$27</p>
+            <p className="text-gray-500">/day</p>
+          </div>
+          <div className="border grow text-center rounded-md py-1.5">
+            <p className="text-xl font-medium">$72</p>
+            <p className="text-gray-500">/week</p>
+          </div>
+          <div className="border grow text-center rounded-md py-1.5">
+            <p className="text-xl font-medium">$365</p>
+            <p className="text-gray-500">/month</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function OwnerInfo() {
+  return (
+    <Card className="flex flex-row items-start max-md:flex-col mt-10">
+      <div className="flex items-center">
+        <Avatar>
+          <AvatarImage
+            src="https://github.com/shadcn.png"
+            alt="@shadcn"
+            className="size-15 rounded-[100px]"
+          />
+        </Avatar>
+        <CardContent>
+          <CardTitle className="text-lg">Owned by Shan R</CardTitle>
+          <CardAction className="mt-3">
+            <Button>Message</Button>
+          </CardAction>
+        </CardContent>
+      </div>
+      <div className="">
+        <p className="mb-3 font-medium">Approximate location:</p>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1472086.7749927582!2d-80.61774836557551!3d43.893756423188556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2a1d7471156d%3A0x4ecad8e272e4c2a2!2sGreater%20Toronto%20Area%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sin!4v1767149734066!5m2!1sen!2sin"
+          width="400"
+          height="350"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </Card>
+  );
 }
 
 const Listing = () => {
   return (
     <section>
-        <SearchBox />
-        <section className="flex max-md:flex-col border border-red-400 max-w-6xl mx-auto h-screen mt-10 justify-between max-xl:px-5 gap-5">
-             <ListingImage />
-             <ListingInfo />
-        </section>
+      <SearchBox />
+      <section className="max-w-6xl mx-auto my-10  max-xl:px-5">
+        <div className="flex gap-5 max-md:flex-col justify-between">
+          <ListingImage />
+          <ListingInfo />
+        </div>
+        <OwnerInfo />
+      </section>
     </section>
-  )
-}
+  );
+};
 
-export default Listing
+export default Listing;
